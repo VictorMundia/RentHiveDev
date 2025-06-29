1f2377d826820a6e86095e2d5a621f151aac91f8
Victor <victor.mundia@strathmore.edu>
2025-06-29 10:57:34 +0300

    urls

--- renthive/properties/views.py
+++ renthive/properties/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Property, Unit
+from .forms import PropertyForm, UnitForm
 
-# Create your views here.
+@login_required
+def property_list(request):
+    properties = Property.objects.filter(owner=request.user)
+    return render(request, 'properties/property_list.html', {'properties': properties})
+
+@login_required
+def property_detail(request, pk):
+    property = get_object_or_404(Property, pk=pk, owner=request.user)
+    units = property.units.all()
+    return render(request, 'properties/property_detail.html', {
+        'property': property,
+        'units': units,
+    })
+
+@login_required
+def property_create(request):
+    if request.method == 'POST':
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            property = form.save(commit=False)
+            property.owner = request.user
+            property.save()
+            messages.success(request, 'Property created successfully!')
+            return redirect('properties:property_list')
+    else:
+        form = PropertyForm()
+    return render(request, 'properties/property_form.html', {'form': form})
+
+@login_required
+def property_update(request, pk):
+    property = get_object_or_404(Property, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        form = PropertyForm(request.POST, instance=property)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Property updated successfully!')
+            return redirect('properties:property_list')
+    else:
+        form = PropertyForm(instance=property)
+    return render(request, 'properties/property_form.html', {'form': form})
+
+@login_required
+def property_delete(request, pk):
+    property = get_object_or_404(Property, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        property.delete()
+        messages.success(request, 'Property deleted successfully!')
+        return redirect('properties:property_list')
+    return render(request, 'properties/property_confirm_delete.html', {'property': property})
