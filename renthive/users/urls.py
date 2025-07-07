c8637507d036dee2687fc37c9bc2313c52f7592e
Victor <victor.mundia@strathmore.edu>
2025-07-07 07:04:03 +0300

    Password Reset

--- renthive/users/urls.py
+++ renthive/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
@@ -13,4 +14,8 @@ urlpatterns = [
     path('card-payment/', views.card_payment, name='card_payment'),
     path('bank-transfer/', views.bank_transfer, name='bank_transfer'),
     path('owner/bank-details/', views.owner_bank_details, name='owner_bank_details'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
